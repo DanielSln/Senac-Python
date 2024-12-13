e4b1a4b2d2b3e3a5ab972a5ba4bb64b4250faa32
Daniel Solano <12danielsolano@gmail.com>
2024-12-13 19:24:55 -0300

    Add files via upload

---
+++ Atv1/Atv1_Ex2.py
@@ -0,0 +1,7 @@
+#Calculadora de três números
+a = int(input("Insira o primeiro número: "))
+b = int(input("Insira o segundo número: "))
+c = int(input("Insira o terceiro número: "))
+soma = a + b + c
+print("A soma é: ",(soma))
+
